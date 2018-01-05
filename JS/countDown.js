adb42d4e0dbbf8b10e8038814aeb6df93006d03b
User <user@Users-Mac-mini.local>
2018-01-04 22:02:57 -0800

    Created the setInterval Function with the Day, Hours, Minutes and Seconds

--- JS/countDown.js
+++ JS/countDown.js
@@ -1,1 +1,34 @@
-var end = new Date();
+
+var end = new Date('June 8 2018 08:00 am');
+
+
+var sec = 1000;
+var min = sec * 60;
+var hrs = min * 60;
+var day = hrs * 24;
+
+
+var interval = setInterval(myTimer, 1000);
+
+function myTimer(){
+
+	var now = new Date();
+
+	var countdown = end.getTime() - now.getTime();
+	
+	var days = (Math.floor(countdown / day));
+	var hours = (Math.floor((countdown % day) / hrs));
+	var minutes = (Math.floor((countdown % hrs ) / min));
+	var seconds = (Math.floor((countdown % min ) / sec));
+
+	document.getElementById("display").innerHTML = 
+	days + " days  " + hours + "  hrs  " + minutes + "  min  " + seconds + "  sec";
+
+	if (countdown < 0){
+		document.getElementById("display").innerHTML = "Game Time!"
+	}
+
+}
+
+
+
