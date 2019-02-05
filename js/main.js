909f7cb1c1eab410e4726b511e5bb3d7bcbeefc5
User <mirantrix80@gmail.com>
2019-02-04 20:17:16 -0800

    Added section and content to the home page

--- js/main.js
+++ js/main.js
@@ -2,28 +2,72 @@
 
 (function getLocalGames(team){
 	var ul = document.getElementById('upCommingGames');
+	var ul2 = document.getElementById('unConfirmed');
+
 	for(var i = 0; i < games.length; i++){
-		if(games[i].local == team || games[i].visiting == team){
 
-			var match = games[i].local + " vs " + games[i].visiting;
-			var match_at = games[i].match_at;
+		if((games[i].local == team || games[i].visiting == team) && (games[i].local !== null && games[i].visiting !== null)){
 
+			let match_at = formatDate(games[i].match_at);
+			let match = games[i].local + " vs " + games[i].visiting;
+			let match_tournament = games[i].tournament;
+			let match_city = games[i].city;
 
-			var li = document.createElement('li');
-			var p1 = document.createElement('p');
-			var p2 = document.createElement('p');
-			p1.innerHTML = match;
-			p2.innerHTML = match_at;
-			li.appendChild(p1);
+			let li = document.createElement('li');
+			let h3 = document.createElement('h3');
+			let p2 = document.createElement('p');
+			let p3 = document.createElement('p');
+			let p4 = document.createElement('p');
+
+			h3.innerHTML = match_at;
+			p2.innerHTML = match;
+			p3.innerHTML = match_tournament;
+			p4.innerHTML = match_city;
+
+			li.appendChild(h3);
 			li.appendChild(p2);
+			li.appendChild(p3);
+			li.appendChild(p4);
 			ul.appendChild(li);
+
+
+		} else {
+
+			let match_at = formatDate(games[i].match_at);
+			let match_tournament = games[i].tournament;
+			let match_city = games[i].city;
+
+			let li = document.createElement('li');
+			let h3 = document.createElement('h3');
+			let p2 = document.createElement('p');
+			let p3 = document.createElement('p');
+			let p4 = document.createElement('p');
+
+			h3.innerHTML = match_at;
+			p3.innerHTML = match_tournament;
+			p4.innerHTML = match_city;
+
+			li.appendChild(h3);
+			li.appendChild(p3);
+			li.appendChild(p4);
+			ul2.appendChild(li);
+
 		}
 	}
-})("JAM");
+})("MEX");
 
 
 
 // Display Date
+
+function formatDate(date){
+	var value = (new Date(date)).toString();
+	var array = value.split(" ").splice(0,4);
+	console.log(array[1] + " " + array[2] + " " + array[3]);
+	return (array[1] + " " + array[2] + " " + array[3]);
+}
+
+
 function displayDate(){
 
 
