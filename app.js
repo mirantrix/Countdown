4185dfe4b54b867743cabaea6a98c453adc5f0ee
User <mirantrix80@gmail.com>
2019-03-09 19:47:18 -0800

    Add public directory to serve static assets

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use('/public', express.static('public'));
+
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
