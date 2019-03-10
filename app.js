c59972338449e701dd44688659a2cca4b21619e2
User <mirantrix80@gmail.com>
2019-03-10 13:36:45 -0700

    create ROUTE for matches

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-  
+const matchesRoute = require('./controllers/matchesRoute');
+
 app.set('view engine', 'pug');
 
 app.use('/public', express.static('public'));
@@ -9,5 +10,7 @@ app.use('/public', express.static('public'));
 app.get('/', (req, res) => {
   res.render('index', {title: 'Hello World', message: 'Hello World'});
 });
+
+app.use('/matches', matchesRoute);
 
 app.listen(port, () => console.log(`Port: ${port}`));
