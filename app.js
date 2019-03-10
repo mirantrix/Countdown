1ec124f1551e891e68f6f20ea951db44339bf4be
User <mirantrix80@gmail.com>
2019-03-09 19:09:02 -0800

    install pug dependency, set view engine and views directory

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+  
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.render('index', {title: 'Hello World', message: 'Hello World'});
 });
 
 app.listen(port, () => console.log(`Port: ${port}`));
