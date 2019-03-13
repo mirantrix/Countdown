0267ecfa302814425cc054de770a3d167935df69
User <mirantrix80@gmail.com>
2019-03-13 12:21:49 -0700

    Erase standard, Setup EsLint + Prettier

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const matchesRoute = require('./controllers/matchesRoute')
+const express = require('express');
+const matchesRoute = require('./controllers/matchesRoute');
 
-app.set('view engine', 'pug')
+const app = express();
+const port = 8080;
 
-app.use('/public', express.static('public'))
+app.set('view engine', 'pug');
+
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hello World', message: 'Hello World' })
-})
+    res.render('index', { title: 'Hello World', message: 'Hello World' });
+});
 
-app.use('/matches', matchesRoute)
+app.use('/matches', matchesRoute);
 
-app.listen(port, () => console.log(`Port: ${port}`))
+app.listen(port, () => console.log(`Port: ${port}`));
