4d580f19d1ef100de0be704a260bd327936dbd67
User <mirantrix80@gmail.com>
2019-03-29 12:45:48 -0700

    [Style]: Formatting Function callers and calees

--- client/js/populateHTML.js
+++ client/js/populateHTML.js
@@ -1,17 +1,11 @@
 
+// Refactor Data Param
 function populateHTML(matches, data) {
+  matches.map(toDOM);
   jumbotron(data.media.jumbotron);
   populateProximoPartido();
   populateIntroCard();
-  matches.map(toDOM);
   populateFooter(data.fmf, data.footer);
-}
-
-
-// Refactor
-function jumbotron(image) {
-  const jumbotron = getId('jumbotron');
-  jumbotron.innerHTML = '<img src=' + '" ' + image + '.gif' + '" ' + 'alt="">';
 }
 
 function toDOM({local, visiting, match_date, stadium, city, tournament}, index) {
@@ -25,8 +19,8 @@ function toDOM({local, visiting, match_date, stadium, city, tournament}, index) 
   const match = setTag('div');
   const dateAndMatchDiv = setTag('div');
 
-  match.className = "match";
-  dateAndMatchDiv.className = "match-card";
+  match.className = 'match';
+  dateAndMatchDiv.className = 'match-card';
 
   const getTeams = setLocalAndVisitingTeamDiv(local, visiting);
   const getDate = setMatchDateDiv(match_date);
@@ -38,6 +32,14 @@ function toDOM({local, visiting, match_date, stadium, city, tournament}, index) 
   match.appendChild(getMoreInfo);
   return matchCards.appendChild(match);
 }
+
+function setTag(name){
+  return document.createElement(name);
+};
+
+function getId(name){
+  return document.getElementById(name);
+};
 
 function countdown(nextMatch) {
   getCountdownClock();
@@ -72,35 +74,6 @@ function countdown(nextMatch) {
   setInterval(startCountdown,perSecond);
 }
 
-
-function appendDateFormat(dateFormatId, literal, innerText) {
-  const clock = getId('countdown-clock');
-  const timerBlocks = setTag('div');
-  const format = setTag('p');
-  const timeIndicator = setTag('p');
-  const symbolBlock = setTag('div');
-  const timeSymbol = setTag('p');
-
-  format.id = dateFormatId;
-
-  timerBlocks.className = 'timer-blocks';
-  format.className = 'time-value';
-  timeIndicator.className = 'time-indicator';
-  symbolBlock.className = 'timer-blocks';
-  timeSymbol.className = 'time-symbol';
-
-  timeIndicator.innerHTML = innerText;
-  timeSymbol.innerHTML = literal;
-
-  timerBlocks.appendChild(format);
-  timerBlocks.appendChild(timeIndicator);
-  symbolBlock.appendChild(timeSymbol);
-
-  clock.appendChild(timerBlocks);
-  if(dateFormatId !== 'seconds') clock.appendChild(symbolBlock);
-}
-
-
 function getCountdownClock() {
 
   const days = ['days', '.', 'DÍAS'];
@@ -131,18 +104,45 @@ function getCountdownClock() {
   appendDateFormat(...seconds);
 }
 
+function appendDateFormat(dateFormatId, literal, innerText) {
+  const clock = getId('countdown-clock');
+  const timerBlocks = setTag('div');
+  const format = setTag('p');
+  const timeIndicator = setTag('p');
+  const symbolBlock = setTag('div');
+  const timeSymbol = setTag('p');
+
+  format.id = dateFormatId;
+
+  timerBlocks.className = 'timer-blocks';
+  format.className = 'time-value';
+  timeIndicator.className = 'time-indicator';
+  symbolBlock.className = 'timer-blocks';
+  timeSymbol.className = 'time-symbol';
+
+  timeIndicator.innerHTML = innerText;
+  timeSymbol.innerHTML = literal;
+
+  timerBlocks.appendChild(format);
+  timerBlocks.appendChild(timeIndicator);
+  symbolBlock.appendChild(timeSymbol);
+
+  clock.appendChild(timerBlocks);
+  if(dateFormatId !== 'seconds') clock.appendChild(symbolBlock);
+}
+
 function setLocalAndVisitingTeamDiv(local, visiting) {
   const match = getId('match-cards');
   const div = setTag('div');
   const ul = setTag('ul');
   const versus = setTag('li');
 
-  div.className = "match-teams";
-  versus.className = "versus";
+  div.className = 'match-teams';
+  versus.className = 'versus';
 
   const getLocalTeam = setTeam(local);
   const getVisitingTeam =  setTeam(visiting);
-  versus.innerHTML = "VS";
+  versus.innerHTML = 'VS';
 
   ul.appendChild(getLocalTeam);
   ul.appendChild(versus);
@@ -158,10 +158,9 @@ function setTeam({abbreviation, flag, status}) {
   const teamIcon = setTag('img');
   const h3 = setTag('h3');
 
-  teamStatus.className = status;
-  teamStatus.className += " " + "team-status";
-  teamIcon.className = "team-icon";
-  teamIcon.src = flag + ".png";
+  teamStatus.className = `${status} team-status`;
+  teamIcon.className = 'team-icon';
+  teamIcon.src = `${flag}.png`;
 
   h3.innerHTML = abbreviation;
 
@@ -172,6 +171,7 @@ function setTeam({abbreviation, flag, status}) {
   return teamStatus;
 }
 
+
 function setMatchDateDiv(match_date) {
   const {getMonth, getDate, getDay, getHours, getMinutes} = parseDate(match_date);
   const matchCards = getId('match-cards');
@@ -180,14 +180,14 @@ function setMatchDateDiv(match_date) {
   const date = setTag('p');
   const time = setTag('p');
 
-  div.className = "match-date";
-  month.className = "month";
-  date.className = "date";
-  time.className = "time";
+  div.className = 'match-date';
+  month.className = 'month';
+  date.className = 'date';
+  time.className = 'time';
 
   month.innerHTML = getMonth.toUpperCase();
   date.innerHTML = getDate;
-  time.innerHTML = getDay.substring(0, 3) + ", " + hoursConvertion(getHours).time + ":" + getMinutes + " " + hoursConvertion(getHours).amPm;
+  time.innerHTML = `${getDay.substring(0, 3)}, ${hoursConvertion(getHours).time}:${getMinutes} ${hoursConvertion(getHours).amPm}`;
 
   div.appendChild(month);
   div.appendChild(date);
@@ -196,30 +196,9 @@ function setMatchDateDiv(match_date) {
   return div;
 }
 
-function setMoreInfoDiv(info) {
-  const {stadium, city, tournament} = info;
-  const div = setTag('div');
-  const tournamentData = setTag('p');
-  const cityData = setTag('p');
-
-  div.className = "complementary-info";
-  tournamentData.className = "tournament";
-  cityData.className = "city";
-
-  tournamentData.innerHTML = tournament;
-  cityData.innerHTML = '@ ' + city;
-
-  div.appendChild(tournamentData);
-  div.appendChild(cityData);
-
-  return div;
-}
-
-
-
 function parseDate(anyDate) {
-  const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
-  const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octurbe", "Noviembre", "Diciembre"]
+  const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+  const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octurbe', 'Noviembre', 'Diciembre']
   const setMinutesToMilliseconds = 60000;
 
   const date = new Date(anyDate);
@@ -252,59 +231,38 @@ function hoursConvertion(time) {
   if(time > 12) {
     return {
       time: time - 12,
-      amPm: "pm"
+      amPm: 'pm'
     }
   }
   return {
     time: time,
-    amPm: "am"
+    amPm: 'am'
   }
 }
 
-function splitDate(input) {
+function setMoreInfoDiv({stadium, city, tournament}) {
+  const div = setTag('div');
+  const tournamentData = setTag('p');
+  const cityData = setTag('p');
 
-  const newDate = new Date(input).toUTCString();
+  div.className = 'complementary-info';
+  tournamentData.className = 'tournament';
+  cityData.className = 'city';
 
-  function dateToArray(dateInput) {
-    const dateToStringToArray = dateInput.toString().split(" ");
-    const [day, month, date, fullYear, time] = dateToStringToArray;
-    const timeArray = time.split(":");
-    const [hours, minutes, seconds] = timeArray;
-    return {day, month, date, fullYear, time, hours, minutes, seconds};
-  }
+  tournamentData.innerHTML = tournament;
+  cityData.innerHTML = `@ ${city}`;
 
-  const {day, month, date, fullYear, time, hours, minutes, seconds} = dateToArray(newDate);
+  div.appendChild(tournamentData);
+  div.appendChild(cityData);
 
-  const getDay =  day;
-  const getMonth =  month;
-  const getDate =  date;
-  const getFullYear = fullYear;
-  const getHours = hours;
-  const getMinutes = minutes;
-  const getSeconds = seconds;
-  const getTime = getHours + ":" + getMinutes;
-  const getFullTime = getHours + ":" + getMinutes + ":" + getSeconds;
-
-  const splitedDate = {
-    getDay,
-    getMonth,
-    getFullYear,
-    getDate,
-    getHours,
-    getMinutes,
-    getTime,
-    getFullTime
-  };
-
-  return splitedDate;
+  return div;
 }
 
-
-
-function populateIntroCard(fmf) {
-  const introCard = getId('intro-card');
-  const introCardSection = createIntroCard('Calendario:', 'Más Partidos!');
-  introCard.appendChild(introCardSection);
+function jumbotron(image) {
+  const jumbotron = getId('jumbotron');
+  const jumbotronImage = setTag('img');
+  jumbotronImage.src = `${image}.gif`;
+  jumbotron.appendChild(jumbotronImage);
 }
 
 function populateProximoPartido() {
@@ -314,16 +272,15 @@ function populateProximoPartido() {
 }
 
 function createIntroCard(title, subTitle) {
-
   const matchCards = getId('intro-card');
   const container = setTag('div');
   const span = setTag('span');
   const h2 = setTag('h2');
   const p = setTag('p');
 
-  container.className = "intro container col-90";
-  h2.className = "";
-  span.className = "col-10 mt-xs";
+  container.className = 'intro container col-90';
+  h2.className = '';
+  span.className = 'col-10 mt-xs';
 
   h2.innerHTML = title;
   p.innerHTML = subTitle;
@@ -335,27 +292,25 @@ function createIntroCard(title, subTitle) {
   return container;
 }
 
+function populateIntroCard(fmf) {
+  const introCard = getId('intro-card');
+  const introCardSection = createIntroCard('Calendario:', 'Más Partidos!');
+  introCard.appendChild(introCardSection);
+}
 
 function populateFooter(team, footerData) {
-  const {name, description, avatar, setTags} = team;
-  const {year, legal } = footerData;
-
   const footer = getId('footer');
-
   const footer_brand = createFooterOwner(team);
   const footer_legal = createFooterLegal(footerData);
-
   footer.appendChild(footer_brand);
   footer.appendChild(footer_legal);
 
   return footer;
 }
 
-
 function createFooterOwner({name, description, avatar}) {
-
   const section = setTag('section');
-  section.className = "footer container col-90";
+  section.className = 'footer container col-90';
 
   const matchCards = getId('footer');
   const container = setTag('div');
@@ -365,11 +320,11 @@ function createFooterOwner({name, description, avatar}) {
   const h3 = setTag('h3');
   const p = setTag('p');
 
-  container.className = "footer-brand";
-  div.className = "footer-brand-name";
-  p.className = "siSePuede";
+  container.className = 'footer-brand';
+  div.className = 'footer-brand-name';
+  p.className = 'siSePuede';
 
-  img.src = avatar + ".png";
+  img.src = `${avatar}.png`;
   h3.innerHTML = name;
   p.innerHTML = description;
 
@@ -390,10 +345,10 @@ function createFooterLegal({year, legal}) {
   const legal_year = setTag('p');
   const disclaimer_informative = setTag('p');
 
-  section.className = "container col-90";
-  container.className = "footer-legal";
-  legal_year.className = "legal-year";
-  disclaimer_informative.className = "disclaimer-informative";
+  section.className = 'container col-90';
+  container.className = 'footer-legal';
+  legal_year.className = 'legal-year';
+  disclaimer_informative.className = 'disclaimer-informative';
 
   legal_year.innerHTML = year;
   disclaimer_informative.innerHTML = legal;
@@ -404,39 +359,3 @@ function createFooterLegal({year, legal}) {
 
   return section;
 }
-
-function getGameEmojis() {
-  const container = setTag('div');
-  const figure1 = setTag('figure');
-  const figure2 = setTag('figure');
-  const figure3 = setTag('figure');
-  const img1 = setTag('img');
-  const img2 = setTag('img');
-  const img3 = setTag('img');
-
-  container.className = "emoji-container";
-  container.className += " " + "container";
-  img1.className = "game-emojis";
-  img2.className = "game-emojis";
-  img3.className = "game-emojis";
-  img.src = emoji + ".png";
-
-  h3.innerHTML = abbreviation;
-
-  figure1.appendChild(img1);
-  figure2.appendChild(img2);
-  figure3.appendChild(img3);
-  container.appendChild(figure1);
-  container.appendChild(figure2);
-  container.appendChild(figure3);
-
-  return container;
-}
-
-function setTag(name){
-  return document.createElement(name);
-};
-
-function getId(name){
-  return document.getElementById(name);
-};
